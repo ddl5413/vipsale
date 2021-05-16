1c47df583b601cc2b195214b8cd6cdec82313d3e
ddl5413 <33371533+ddl5413@users.noreply.github.com>
2021-05-16 17:46:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
+import json
 import time
+import datetime
 # http://chromedriver.chromium.org/
-item_list = [('ZHG5630000', 6), ('LSFY210002', 1)]
+option = {}
 
 
 def launch():
@@ -11,8 +13,9 @@ def launch():
 
 
 def login(browser):
-    browser.find_element_by_id('txtUserID').send_keys('elcfin129')
-    browser.find_element_by_id('txtPwd').send_keys('571262')
+    user = option['user']
+    browser.find_element_by_id('txtUserID').send_keys(user['account'])
+    browser.find_element_by_id('txtPwd').send_keys(user['password'])
     check_code = input('输入验证码:')
     browser.find_element_by_id('txtVerify').send_keys(check_code)
     browser.find_element_by_id('btnSignIn').click()
@@ -21,44 +24,92 @@ def login(browser):
 
 
 def try_to_buy(browser):
-    for item in item_list:
-        sku = item[0]
-        want_qty = item[1]
-        # 输入数量检索
-        browser.find_element_by_id('txtSearch').clear()
-        browser.find_element_by_id('txtSearch').send_keys(sku)
-        browser.find_element_by_id('btnSearch').click()
-        time.sleep(1)
-        while True:
-            alert_text = ''
-            # 获取库存
-            qty = browser.find_element_by_xpath(f'//*[@id="{sku}"]/table/tbody/tr[2]/td[1]').text
-            print(f"{sku}数量:{qty}")
-            qty = int(qty)
-            if qty > 0:
-                order_count = browser.find_element_by_xpath(f'//*[@id="{sku}"]/table/tbody/tr[2]/td[2]/input')
-                order_count.clear()
-                if want_qty > qty:
-                    want_qty = qty
-                order_count.send_keys(want_qty)
-                # 点击 加入购物车
-                browser.find_element_by_xpath(f'//*[@id="{sku}"]/table/tbody/tr[3]/td/input').click()
-                for i in range(5):
-                    try:
-                        alert = browser.switch_to_alert()
-                        alert_text = alert.text
-                        print(alert.text)
-                        alert.accept()
-                        break
-                    except Exception as e:
-                        time.sleep(0.1)
-            if alert_text != '本次内卖尚未开始，敬请期待!':
-                break
-            time.sleep(1)
+    item_list = []
+    for item in option['items']:
+        item_list.append((item['sku'], item['want']))
+    # 购买列表非空时无限循环
+    while len(item_list) > 0:
+        now = datetime.datetime.now().strftime('%H:%M:%S')
+        print(f"{now}: 本次循环商品: {item_list}")
+        # 没有买够数量的商品列表
+        item_list_sub = []
+        for item in item_list:
+            success = False
+            while not success:
+                try:
+                    # 输入数量检索
+                    browser.find_element_by_id('txtSearch').clear()
+                    browser.find_element_by_id('txtSearch').send_keys(item[0])
+                    browser.find_element_by_id('btnSearch').click()
+                    time.sleep(1)
+                    want = buy_item(browser, item)
+                    if want > 0:
+                        item_list_sub.append((item[0], want))
+                    success = True
+                except Exception as e:
+                    print(e)
+                    success = False
+                    time.sleep(1)
+        item_list = item_list_sub
+
+
+def buy_item(browser, item):
+    sku = item[0]
+    want = item[1]
+    while True:
+        qty = browser.find_element_by_xpath(f'//*[@id="{sku}"]/table/tbody/tr[2]/td[1]').text
+        print(f"商品: {sku}, 库存: {qty}")
+        qty = int(qty)
+        if qty > 0:
+            # 输入购买数量
+            order_count = browser.find_element_by_xpath(f'//*[@id="{sku}"]/table/tbody/tr[2]/td[2]/input')
+            order_count.clear()
+            buy = min(qty, want)
+            order_count.send_keys(buy)
+            # 点击 加入购物车
+            browser.find_element_by_xpath(f'//*[@id="{sku}"]/table/tbody/tr[3]/td/input').click()
+            alert_text = dismiss_alert(browser)
+            if alert_text == '本次内卖尚未开始，敬请期待!':
+                time.sleep(1)
+                continue
+            elif alert_text == '产品订购成功':
+                print(f'成功购买商品{sku}, {buy}件')
+                # 返回未能满足的求购数量
+                return want - buy
+            else:
+                print(f'商品{sku}存在问题，放弃购买')
+                return 0
+        else:
+            return want
+
+
+def dismiss_alert(browser):
+    alert_text = ''
+    for i in range(5):
+        try:
+            alert = browser.switch_to.alert
+            alert_text = alert.text
+            print(f"alert:{alert.text}")
+            alert.accept()
+            break
+        except Exception as e:
+            time.sleep(0.1)
+    return alert_text
+
+
+def load_config():
+    global option
+    with open("config.json") as config_file:
+        option = json.load(config_file)['option']
 
 
 if __name__ == '__main__':
-    _browser = launch()
-    login(_browser)
-    input('等待... 按回车继续.')
-    try_to_buy(_browser)
+    try:
+        _browser = launch()
+        load_config()
+        login(_browser)
+        input('等待... 按回车继续.')
+        try_to_buy(_browser)
+    finally:
+        input('购买完成... 按回车继续.')
+        _browser.close()
